2776eadc85a2cc6bdc849d38dc2272acb8287e5d
jeff.park <jeff.park@machbase.com>
2023-03-28 17:56:30 +0900

    ✨ 캔버스 구현

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,62 +1,102 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components'
+import { useState, useRef, useLayoutEffect, useEffect } from "react";
+import styled from "styled-components";
 
 interface CanvasProps {
-	width: number;
-	height: number;
+  width: number;
+  height: number;
 }
 
 export const Canvas = (props: CanvasProps) => {
-	const { width, height } = props;
-	const [board, setBoard] = useState(create2DArray(width, height));
+  const { width, height } = props;
+  const [board, setBoard] = useState(create2DArray(height, width));
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerHeight, setContainerHeight] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const maxTileSize = 40;
 
-	const boardWidth = 100/width;
-	const boardHeight = 100/height;
+  useLayoutEffect(() => {
+    if (containerRef.current) {
+      setContainerWidth(containerRef.current.clientWidth);
+      setContainerHeight(containerRef.current.clientHeight);
+    }
+  }, []);
 
-	return (
-		<Conatiner>
-			{
-				board.map((v) => {
-					return (
-						<div style={{width: '100%', height: boardHeight+'%', display: 'flex'}}>
-							{
-								v.map(() => {
-									return (
-										<Pixel width={boardWidth} height={100}/>
-									)
-								})
-							}
-						</div>
-					)
-				})
-			}
-		</Conatiner>
-	)
-}
+  // width resize
+  useEffect(() => {
+    function handleWindowResize() {
+      if (containerRef.current) {
+        setContainerWidth(containerRef.current.clientWidth);
+        setContainerHeight(containerRef.current.clientHeight);
+      }
+    }
 
-function create2DArray(x:number, y:number) {
-	const newArr = [];
-	for (let i = 0; i < x; i++) {
-		newArr[i] = new Array(y).fill(0);
-	}
-	return newArr;
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const boardWidth =
+    width >= height ? containerWidth / width : containerHeight / height;
+  const boardHeight =
+    width >= height ? containerWidth / height : containerHeight / height;
+
+  return (
+    <Conatiner ref={containerRef}>
+      <Wrapper style={{ justifyContent: width < height ? "center" : "" }}>
+        {board.map((v) => {
+          return (
+            <div
+              style={{
+                width: "100%",
+                height: boardWidth + "px",
+                maxHeight: maxTileSize + "px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              {v.map(() => {
+                return <Pixel width={boardWidth} maxSize={maxTileSize} />;
+              })}
+            </div>
+          );
+        })}
+      </Wrapper>
+    </Conatiner>
+  );
+};
+
+function create2DArray(x: number, y: number) {
+  const newArr = [];
+  for (let i = 0; i < x; i++) {
+    newArr[i] = new Array(y).fill(0);
+  }
+  return newArr;
 }
 
 const Conatiner = styled.div`
-	width: 100%;
-	height: 100%;
+  width: 100%;
+  height: 100%;
 
-	display: flex;
-	align-items: start;
-	justify-content: start;
-	flex-wrap: wrap;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-	background-color: #2a2a2a;
-`
+  background-color: #2a2a2a;
+`;
 
-const Pixel = styled.div<{width: number, height: number}>`
-	width: ${({width}) => width+'%'};
-	height: ${({width}) => width+'%'};
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
 
-	background-color: aliceblue;
-`
+const Pixel = styled.div<{ width: number; maxSize: number }>`
+  max-width: ${({ maxSize }) => maxSize + "px"};
+  max-height: ${({ maxSize }) => maxSize + "px"};
+  width: ${({ width }) => width + "px"};
+  height: ${({ width }) => width + "px"};
+
+  background-color: aliceblue;
+  border: 0.5px solid;
+`;
--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,44 +1,41 @@
-import styled from 'styled-components'
-import { Canvas } from '@/components/Canvas'
+import styled from "styled-components";
+import { Canvas } from "@/components/Canvas";
 
 export default function DefaultLayout() {
-	return (
-		<Body>
-			<CanvasCotainer>
-				<Canvas width={15} height={13}/>
-			</CanvasCotainer>
-			<MenuContainer>
-
-			</MenuContainer>
-		</Body>
-	)
+  return (
+    <Body>
+      <CanvasCotainer>
+        <Canvas width={100} height={120} />
+      </CanvasCotainer>
+      <MenuContainer></MenuContainer>
+    </Body>
+  );
 }
 const Body = styled.div`
-	width: 100%;
-	min-height: 100vh;
-	height: auto;
+  width: 100%;
+  min-height: 100vh;
+  height: auto;
 
-	display: flex;
-
-`
+  display: flex;
+`;
 
 const CanvasCotainer = styled.div`
-	// TODO min width setting
-	/* min-width: ; */
-	width: 70%;
-	min-height: 100vh;
-	height: auto;
+  // TODO min width setting
+  /* min-width: ; */
+  width: 70%;
+  min-height: 100vh;
+  height: auto;
 
-	padding: 2rem;
+  padding: 2rem;
 
-	background-color: lightgray;
-`
+  background-color: lightgray;
+`;
 
 const MenuContainer = styled.div`
-	// TODO min width setting
-	/* min-width: ; */
-	width: 30%;
-	height: 100vh;
+  // TODO min width setting
+  /* min-width: ; */
+  width: 30%;
+  height: 100vh;
 
-	padding: 1rem;
-`
+  padding: 1rem;
+`;
