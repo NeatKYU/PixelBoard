f1c3f46d3e29c3cd3676ccb7f13d39b830637e80
jeff.park <jeff.park@machbase.com>
2023-03-29 16:19:06 +0900

    💄 그리드 스타일 수정

--- src/components/CanvasGrid.tsx
+++ src/components/CanvasGrid.tsx
@@ -1,85 +1,101 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState } from 'react';
 
 interface CanvasGridProps {
-  rows: number;
-  columns: number;
-  cellSize: number;
+    rows: number;
+    columns: number;
+    cellSize: number;
 }
 
 export default function CanvasGrid(props: CanvasGridProps) {
-  const { rows, columns, cellSize } = props;
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [canvasWidth, setCanvasWidth] = useState<number>(0);
-  const [canvasHeight, setCanvasHeight] = useState<number>(0);
-  
-  const handleResize = () => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      setCanvasWidth(canvas.offsetWidth);
-      setCanvasHeight(canvas.offsetHeight);
-      const ctx = canvas.getContext('2d');
-      if(ctx) {
-        for (let i = 0; i < rows; i++) {
-          for (let j = 0; j < columns; j++) {
-            console.log('stroke')
-            ctx.strokeStyle = '#262626';
-            ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
-          }
+    const { rows, columns, cellSize } = props;
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [canvasWidth, setCanvasWidth] = useState<number>(0);
+    const [canvasHeight, setCanvasHeight] = useState<number>(0);
+
+    const handleResize = () => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            setCanvasWidth(canvas.offsetWidth);
+            setCanvasHeight(canvas.offsetHeight);
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                //TODO 원래 색칠한 것 갖고있어야함
+                for (let i = 0; i < rows; i++) {
+                    for (let j = 0; j < columns; j++) {
+                        ctx.strokeStyle = '#eeeeeeb3';
+                        ctx.strokeRect(
+                            j * cellSize,
+                            i * cellSize,
+                            cellSize,
+                            cellSize
+                        );
+                    }
+                }
+            }
         }
-      }
-    }
-  };
-
-  useEffect(() => {
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
     };
-  }, [canvasRef, rows, columns, cellSize]);
 
-  // useEffect(() => {
-  //   const canvas = canvasRef.current;
-  //   if (canvas) {
-  //     const ctx = canvas.getContext("2d");
-  //     if (ctx !== null) {
-  //       for (let i = 0; i < rows; i++) {
-  //         for (let j = 0; j < columns; j++) {
-  //           ctx.fillStyle = "black";
-  //           ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }, [canvasRef.current, canvasWidth, canvasHeight, cellSize, rows, columns]);
+    useEffect(() => {
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [canvasRef, rows, columns, cellSize]);
 
-  const handleCellClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const ctx = canvas.getContext("2d");
-      if (ctx) {
-        console.log('event', event.nativeEvent)
-        const x = Math.floor((event.nativeEvent.offsetX / canvasWidth) * columns);
-        const y = Math.floor((event.nativeEvent.offsetY / canvasHeight) * rows);
-        ctx.fillStyle = "white";
-        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-      }
-    }
-  };
+    // useEffect(() => {
+    //   const canvas = canvasRef.current;
+    //   if (canvas) {
+    //     const ctx = canvas.getContext("2d");
+    //     if (ctx !== null) {
+    //       for (let i = 0; i < rows; i++) {
+    //         for (let j = 0; j < columns; j++) {
+    //           ctx.fillStyle = "black";
+    //           ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+    //         }
+    //       }
+    //     }
+    //   }
+    // }, [canvasRef.current, canvasWidth, canvasHeight, cellSize, rows, columns]);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      width={cellSize * columns}
-      height={cellSize * rows}
-      onClick={handleCellClick}
-      style={{maxWidth: '100%', maxHeight: '100%', background: 'black'}}
-    />
-  );
+    const handleCellClick = (event: React.MouseEvent<HTMLCanvasElement>) => {
+        const canvas = canvasRef.current;
+        if (canvas) {
+            const ctx = canvas.getContext('2d');
+            if (ctx) {
+                console.log('event', event.nativeEvent);
+                const x = Math.floor(
+                    (event.nativeEvent.offsetX / canvasWidth) * columns
+                );
+                const y = Math.floor(
+                    (event.nativeEvent.offsetY / canvasHeight) * rows
+                );
+                ctx.fillStyle = 'white';
+                ctx.strokeStyle = '#eeeeeeb3';
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+                ctx.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
+        }
+    };
+
+    return (
+        <canvas
+            ref={canvasRef}
+            width={cellSize * columns}
+            height={cellSize * rows}
+            onClick={handleCellClick}
+            style={{
+                maxWidth: '100%',
+                maxHeight: '100%',
+                background: '#afafaf7d',
+            }}
+        />
+    );
 }
 
 CanvasGrid.defaultProps = {
-  cellSize: 10,
-  rows: 16,
-  columns: 16,
-}
+    cellSize: 10,
+    rows: 16,
+    columns: 16,
+};
--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,55 +1,55 @@
 //lib
-import styled from "styled-components";
-import { useRecoilValue } from "recoil";
-import { CanvasHeightState, CanvasWidthState } from "@/atom";
+import styled from 'styled-components';
+import { useRecoilValue } from 'recoil';
+import { CanvasHeightState, CanvasWidthState } from '@/atom';
 
 //components
-import { Canvas } from "@/components/Canvas";
-import CanvasGrid from "@/components/CanvasGrid";
+import { Canvas } from '@/components/Canvas';
+import CanvasGrid from '@/components/CanvasGrid';
 
 export default function DefaultLayout() {
-  const CanvasWidth = useRecoilValue(CanvasWidthState);
-  const CanvasHeight = useRecoilValue(CanvasHeightState);
+    const CanvasWidth = useRecoilValue(CanvasWidthState);
+    const CanvasHeight = useRecoilValue(CanvasHeightState);
 
-  return (
-    <Body>
-      <CanvasCotainer>
-        {/* <Canvas width={CanvasWidth} height={CanvasHeight} /> */}
-        <CanvasGrid cellSize={10} columns={30} rows={30}/>
-      </CanvasCotainer>
-      <MenuContainer></MenuContainer>
-    </Body>
-  );
+    return (
+        <Body>
+            <CanvasCotainer>
+                {/* <Canvas width={CanvasWidth} height={CanvasHeight} /> */}
+                <CanvasGrid cellSize={15} columns={30} rows={15} />
+            </CanvasCotainer>
+            <MenuContainer></MenuContainer>
+        </Body>
+    );
 }
 const Body = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  height: auto;
+    width: 100%;
+    min-height: 100vh;
+    height: auto;
 
-  display: flex;
+    display: flex;
 `;
 
 const CanvasCotainer = styled.div`
-  // TODO min width setting
-  /* min-width: ; */
-  width: 70%;
-  min-height: 100vh;
-  height: auto;
+    // TODO min width setting
+    /* min-width: ; */
+    width: 70%;
+    min-height: 100vh;
+    height: auto;
 
-  padding: 2rem;
+    padding: 2rem;
 
-	display: flex;
-	justify-content: center;
-	align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  background-color: lightgray;
+    background-color: lightgray;
 `;
 
 const MenuContainer = styled.div`
-  // TODO min width setting
-  /* min-width: ; */
-  width: 30%;
-  height: 100vh;
+    // TODO min width setting
+    /* min-width: ; */
+    width: 30%;
+    height: 100vh;
 
-  padding: 1rem;
+    padding: 1rem;
 `;
